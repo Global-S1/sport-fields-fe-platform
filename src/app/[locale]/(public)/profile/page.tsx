c0c59bee0ece520f273d1d92bb3af17f74d36526
JMartinezRs <juan.martinez@globals.one>
2025-07-03 23:37:59 -0500

    feat: 🎸 Add Seo only to Profile page
    ✅ Closes: GLO-750

--- src/app/[locale]/(public)/profile/page.tsx
+++ src/app/[locale]/(public)/profile/page.tsx
@@ -1,4 +1,32 @@
+import { config } from "@/config/config";
 import ProfilePage from "@/modules/customer/profile/pages/profile.page";
+import { PageProps } from "@/shared/interfaces/types";
+import { createTranslator } from "next-intl";
+
+export async function generateMetadata({ params }: PageProps) {
+  const locale = (await params).locale;
+
+  const t = createTranslator({
+    locale,
+    messages: await import(`../../../../../messages/${locale}.json`),
+  });
+
+  return {
+    title: t("profile.metadata.title"),
+    description: t("profile.metadata.description"),
+    alternates: {
+      canonical: `${config.DOMAIN}/${locale}/profile`,
+    },
+    openGraph: {
+      title: t("profile.metadata.title"),
+      description: t("profile.metadata.description"),
+      url: `${config.DOMAIN}/${locale}/`,
+      siteName: "Sport Field",
+      locale,
+      type: "website",
+    },
+  };
+}
 
 export default function AppProfilePage() {
   return <ProfilePage />;
